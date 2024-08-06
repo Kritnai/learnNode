81665b413ade4d2b63f474e9611576f8db32353b
Kritnai <KritsanaiHoisang@gmail.com>
2024-08-06 23:15:14 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+// const router = require('./routes/MyRouter')
+const path = require('path')
+const app = express()
+
+const router = require('./routes/MyRouter')
+
+// use templete
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+
+app.use(router)
+// use static file
+app.use(express.static(path.join(__dirname, 'public')))
+
+// app.use(router)
+app.listen(8080, () => {
+    console.log("starting Server at port 8080")
+})
+// 
+
